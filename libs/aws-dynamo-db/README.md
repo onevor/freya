cb7b9a6faf7afb2f3062a8627647ae55457d552c
Dennis Lien <dennis.lien.o@gmail.com>
2023-03-01 11:57:33 +0100

    Setup dynamo lib: wip

---
+++ libs/aws-dynamo-db/README.md
@@ -0,0 +1,11 @@
+# aws-dynamo-db
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test aws-dynamo-db` to execute the unit tests via [Jest](https://jestjs.io).
+
+## Running lint
+
+Run `nx lint aws-dynamo-db` to execute the lint via [ESLint](https://eslint.org/).
---
+++ libs/aws-dynamo-db/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'aws-dynamo-db',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/aws-dynamo-db',
+};
